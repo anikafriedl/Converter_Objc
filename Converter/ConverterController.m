ae4f738fe3af0298857acf653fd850c18e66242c
Anika Friedl <a.friedl@mad-mobile.de>
2014-11-18 13:36:25 +0100

    implement button

--- Converter/ConverterController.m
+++ Converter/ConverterController.m
@@ -4,7 +4,7 @@
 
 @property (nonatomic, weak) IBOutlet UILabel *displayLabel;
 @property (weak, nonatomic) IBOutlet UILabel *currencyLabel;
-
+@property (nonatomic) BOOL komma;
 @end
 
 @implementation ConverterController {
@@ -13,9 +13,27 @@
 - (void)viewDidLoad {
 	[super viewDidLoad];
 	self.displayLabel.text = @"0";
+    self.komma = false;
 }
 
 - (IBAction)buttonTouched:(id)sender {
+    if (self.displayLabel.text.length <10) {
+        if ((long)[sender tag] == 10) {
+            if (!self.komma) {
+                self.displayLabel.text = [NSString stringWithFormat:@"%@,", self.displayLabel.text];
+                self.komma = true;
+            }
+            
+        }
+        else if ([self.displayLabel.text isEqualToString:@"0"]) {
+            self.displayLabel.text = [NSString stringWithFormat:@"%ld",(long)[sender tag]];
+        }
+        else
+        {
+            self.displayLabel.text = [NSString stringWithFormat:@"%@%ld", self.displayLabel.text,(long)[sender tag]];
+        }
+    }
+    
 }
 
 
